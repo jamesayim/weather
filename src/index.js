6ee98590b3aefcaeae2e2669997b8f2a2f254e6a
jamesyim <theladjames@gmail.com>
2025-02-09 21:33:55 +0000

    Add weather forecast layout and functionality with search feature

--- src/index.js
+++ src/index.js
@@ -1,2 +1,16 @@
 import './styles.css';
 
+const searchButton = document.querySelector('.search-button');
+const searchBar = document.querySelector('.search-bar');
+
+function shiftSearchButton() {
+    if (searchBar.value) {
+        searchButton.classList.add('shifted');
+    }
+    else {
+        searchButton.classList.remove('shifted');
+    }
+}
+
+searchBar.addEventListener('input', shiftSearchButton);
+
