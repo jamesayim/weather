2b01917d486c556c8f81ab28c5555a6e5953fd13
jamesyim <theladjames@gmail.com>
2025-02-12 19:27:47 +0000

    feat: fixed loading bugs, added fetch for five-day forcast

--- src/index.js
+++ src/index.js
@@ -13,7 +13,18 @@ const sunrise = document.querySelector('.sunrise-time');
 const sunset = document.querySelector('.sunset-time');
 
 // Default
-document.addEventListener('DOMContentLoaded', () => fetchData('New York'));
+document.addEventListener('DOMContentLoaded', () => {
+    fetchData('New York');
+});
+
+// Search Button
+searchButton.addEventListener('click', (e) => {
+    if (searchBar.value) {
+        fetchData();
+    } else {
+        alert('Please crosscheck the city name and try again!');
+    }
+});
 
 // Shift Search Button 
 function shiftSearchButton() {
@@ -60,11 +71,31 @@ async function fetchData(defaulLocation) {
     try {
         const location = searchBar.value.toLowerCase() || defaulLocation;
         const currentDate = new Date();
+        const currentWeatherContainer = document.querySelector('.current-weather');
+        const currentWeatherChildren = currentWeatherContainer.children;
+        const fiveDayForcastContainer = document.querySelector('.five-day-forcast-container');
+        const fiveDayForcastChildren = fiveDayForcastContainer.children;
+        const loadingForCW = document.querySelector('.cw-loading');
+        const loadingForF = document.querySelector('.f-loading');
 
-        const fetchedData = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}/${formatDate(currentDate)}/?key=${APIKEY}`, { mode: 'cors' })
+        for (let element of currentWeatherChildren) {
+            if (element.classList.contains('cw-loading')) element.style.display = 'flex';
+            else {
+                element.style.display = 'none';
+            }
+        }
+        for (let element of fiveDayForcastChildren) {
+            if (element.classList.contains('f-loading')) element.style.display = 'flex';
+            else {
+                element.style.display = 'none';
+            }
+        }
+
+        const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}/${formatDate(currentDate)}/?key=${APIKEY}`, { mode: 'cors' })
         .then(async function(response) {
+
             const receivedData = await response.json();
-            console.log(receivedData);
+
             const currentWeatherData = {
                 cityname: receivedData.resolvedAddress,
                 condition: receivedData.currentConditions.conditions,
@@ -82,7 +113,7 @@ async function fetchData(defaulLocation) {
             return currentWeatherData;
         })
         .then(function(data) {
-            cityName.textContent = data.cityname;
+            cityName.textContent = `${ data.cityname}`;
             temp.textContent = data.temperature;
             feelsLike.textContent = data.feelslike;
             condition.textContent = data.condition;
@@ -91,18 +122,78 @@ async function fetchData(defaulLocation) {
             pressure.textContent = data.pressure;
             sunrise.textContent = data.sunrise;
             sunset.textContent = data.sunset;
-            console.log(data);
         })
-        .then(function() {
-            const currentWeatherIcon = document.querySelector('.current-weather-icon');
-            currentWeatherIcon.src = fetchedData.icon;
-            currentWeatherIcon.alt = fetchedData.description;
+        .then(async function() {
+            const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}/?key=${APIKEY}`, { mode: 'cors' });
+            const receivedForcastData = await response.json();
+
+            const days = document.querySelectorAll('.day');
+            const weatherIcons = document.querySelectorAll('.f-weather-icon');
+            const forcastTempDegrees = document.querySelectorAll('.forcast-degree');
+            const forcastConditions = document.querySelectorAll('.forcast-condition');
+
+            console.log(receivedForcastData);
+            const forcastData = {
+                dayOne: 'Next day',
+                dayTwo: 'Next 2 days',
+                dayThree: 'Next 3 days',
+                dayFour: 'Next 4 days',
+                dayFive: 'Next 5 days',
+                
+            }
         })
+        .finally(() => {
+
+            for (let element of currentWeatherChildren) {
+                if (element.classList.contains('cw-loading')) element.style.display = 'none';
+                else {
+                    element.style.display = 'flex';
+                }
+            }
+            for (let element of fiveDayForcastChildren) {
+                if (element.classList.contains('f-loading')) element.style.display = 'none';
+                else {
+                    element.style.display = 'flex';
+                }
+            }
+            if (document.querySelector('.current-weather > h2')) document.querySelector('.current-weather > h2')
+                .style.display = 'block';
+            loadingForCW.style.diplay = 'none';
+            loadingForF.style.diplay = 'none';
+        });
     } catch(error) {
+        showErrorMessage();
+
+        document.querySelector('.current-weather').style.diplay = 'none';
+        document.querySelector('.five-day-forcast-container').style.diplay = 'none';
         console.log(error);
     }
 }
 
+function showErrorMessage() {
+    let errorMessage = document.querySelector('.error-message');
 
+    if (!errorMessage) {
+        errorMessage = document.createElement('span');
+        errorMessage.classList.add('error-message');
+        errorMessage.style.display = 'flex';
+        
+        document.body.appendChild(errorMessage);
+    }
+    errorMessage.textContent = 'An error occured while fetching data. Please enter a valid city, or try again later.';
+    errorMessage.classList.remove('fade-out');
+    errorMessage.classList.add('fade-in');
+
+    setTimeout(() => {
+
+        errorMessage.classList.remove('fade-in');
+        errorMessage.classList.add('fade-out');
+
+            setTimeout(() => {
+                errorMessage.remove();
+            }, 300);
+    }, 3000);
+}
 
 window.fetchData = fetchData;
+
